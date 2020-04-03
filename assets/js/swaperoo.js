eda8f0588f38412495864745a50404a552dbf92e
Simon Laffan <simon@laffan.dev>
2020-04-03 21:21:24 +1100

    apptest:

--- assets/js/swaperoo.js
+++ assets/js/swaperoo.js
@@ -20,7 +20,7 @@ function swap(){
 function tabify(){
     var url = document.getElementById("term").value;
     if (url !== "") {
-        url = "https://tabs.ultimate-guitar.com/search.php?search_type=title&value="+url;
+        url = "ultimate-guitar.com/search.php?search_type=title&value="+url;
         document.getElementById("ug").href = url;
     }
     else{
