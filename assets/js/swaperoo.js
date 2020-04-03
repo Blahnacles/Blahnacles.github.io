83a276eeef9a454291002f30bb758b13d04a1466
Simon Laffan <simon@laffan.dev>
2020-04-03 20:27:22 +1100

    fc

---
+++ assets/js/swaperoo.js
@@ -0,0 +1,29 @@
+function hide() {
+    var desc = document.getElementById("desc")
+    var search = document.getElementById("search")
+    if (desc.style.visibility === "hidden") {
+        desc.style.visibility = "visible";
+        search.style.display = "none";
+    }
+    else {
+        desc.style.visibility = "hidden";
+        search.style.display = "block";
+    }
+}
+function swap(){
+    var desc = document.getElementById("desc");
+    var search = document.getElementById("search");
+    desc.style.display = "none";
+    search.style.display = "block";
+}
+
+function tabify(){
+    var url = document.getElementById("term").value;
+    if (url !== "") {
+        url = "https://www.ultimate-guitar.com/search.php?search_type=title&value="+url;
+        document.getElementById("ug").href = url;
+    }
+    else{
+        document.getElementById("ug").href = "#";
+    }
+}
